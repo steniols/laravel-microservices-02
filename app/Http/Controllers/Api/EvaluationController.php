64642643e87b2f76a1da80ada73c93b7ab6e5c08
Stenio <stenio.dev@planoviver.com>
2024-01-29 18:17:34 -0300

    Add service to connection with microservice-01

--- app/Http/Controllers/Api/EvaluationController.php
+++ app/Http/Controllers/Api/EvaluationController.php
@@ -6,15 +6,18 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreEvaluation;
 use App\Http\Resources\EvaluationResource;
 use App\Models\Evaluation;
+use App\Services\CompanyService;
 use Illuminate\Http\Request;
 
 class EvaluationController extends Controller
 {
     protected $repository;
+    protected $companyService;
 
-    public function __construct(Evaluation $model)
+    public function __construct(Evaluation $model, CompanyService $companyService)
     {
         $this->repository = $model;
+        $this->companyService = $companyService;
     }
     /**
      * Display a listing of the resource.
@@ -32,14 +35,14 @@ class EvaluationController extends Controller
      */
     public function store(StoreEvaluation $request, $company)
     {
-        // $response = $this->companyService->getCompany($company);
-        // $status = $response->status();
-        // if ($status != 200) {
-        //     return response()->json([
-        //         'message' => 'Invalid Company'
-        //     ], $status);
-        // }
-        // $company = json_decode($response->body());
+        $response = $this->companyService->getCompany($company);
+        $status = $response->status();
+        if ($status != 200) {
+            return response()->json([
+                'message' => 'Invalid Company'
+            ], $status);
+        }
+        $company = json_decode($response->body());
 
         $evaluation = $this->repository->create($request->validated());
 
