66e2715929cd0dd69066eef6d19d075df0043357
Stenio <stenio.dev@planoviver.com>
2024-01-30 15:31:13 -0300

    Add feature tests

---
+++ tests/Feature/Api/EvaluatioTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use App\Models\Evaluation;
+use Illuminate\Support\Str;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EvaluatioTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+
+    public function test_get_evaluations_empty(): void
+    {
+        $response = $this->getJson('/evaluations/fake-company');
+
+        $response->assertStatus(200)
+                    ->assertJsonCount(0, 'data');
+    }
+
+    public function test_get_evaluations_company(): void
+    {
+        $company = (string) Str::uuid();
+        $evaluations = Evaluation::factory()->count(6)->create([
+            'company' => $company
+        ]);
+
+        $response = $this->getJson("/evaluations/{$company}");
+
+        $response->assertStatus(200)
+                    ->assertJsonCount(6, 'data');
+    }
+
+    public function test_error_store_evaluation(): void
+    {
+        $company = 'fake-company';
+
+        $response = $this->postJson("/evaluations/{$company}", []);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_store_evaluation(): void
+    {
+        $company = 'fake-company';
+
+        $response = $this->postJson("/evaluations/{$company}", [
+            'company' => (string) Str::uuid(),
+            'comment' => 'New comment',
+            'stars' => 5
+        ]);
+
+        $response->assertStatus(404);
+    }
+}
